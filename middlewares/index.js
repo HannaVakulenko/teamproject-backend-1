51a454444ed186118f4b48db1e0a8a87da3069ad
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-08-09 18:54:00 +0300

    new

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
+const validateBody = require("./validateBody");
 
 module.exports = {
-    isValidId,
-    authenticate,
+  isValidId,
+  authenticate,
+  validateBody,
 };
