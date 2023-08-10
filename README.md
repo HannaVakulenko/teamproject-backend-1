cdebabfc5e985d698b48a800f71a8e982003bcbb
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-08-10 19:44:58 +0300

    new

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # teamproject-backend-1
 
+- User auth for MongoDB:
+  Username: goosetrackteam1
+  Password: De5JbYLYUTsmuCEk
+
+- Connect link for MongoDB:
+  mongodb+srv://goosetrackteam1:De5JbYLYUTsmuCEk@cluster0.ifi4dwj.mongodb.net/
+
+- Link for cloud:
+  https://cloud.mongodb.com/v2/64d2472137eceb06a6b3662e#/metrics/replicaSet/64d2477280ecbd7f310a50c3/explorer/db-goosetrack
 
 This is a team project.
--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -1,15 +1,21 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/auth-controller");
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 const router = express.Router();
 
-// Регистрация
+// SignUp
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
-// Вход
-router.post("/login", validateBody(schemas.loginSchema), ctrl.register);
+// SignIn
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+// isTokenActive
+router.get("/current", authenticate, ctrl.getCurrent);
+
+// Logout
+router.post("/logout", authenticate, ctrl.logout);
 
 module.exports = router;
