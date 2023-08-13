1b53b607bbe90f90e215a8855be93fa9273a2ab1
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-08-13 19:56:24 +0300

    new

--- helpers/index.js
+++ helpers/index.js
@@ -2,10 +2,12 @@ const HttpError = require("./HttpError");
 const handleMongooseError = require("./handleMongooseError");
 const ctrlWrapper = require("./ctrlWrapper");
 const uploadImage = require("./uploadImage");
+const generateAvatar = require("./generateAvatar");
 
 module.exports = {
   HttpError,
   handleMongooseError,
   ctrlWrapper,
   uploadImage,
+  generateAvatar,
 };
